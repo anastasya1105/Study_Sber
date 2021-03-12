c79d9689dbfc104eca7f6647c726e3a85d7b01a3
Pivovarova_Anastasia <serdyuchenko.n@mail.ru>
2021-03-12 10:31:23 +0300

    Delete AccountBalanceException.java

--- src/study/lessonExceptions/MyExceptions/AccountBalanceException.java
+++
@@ -1,8 +0,0 @@
-package study.lessonExceptions.MyExceptions;
-
-public class AccountBalanceException extends AccountExceptions {
-
-    public AccountBalanceException(String message) {
-        super(message);
-    }
-}
