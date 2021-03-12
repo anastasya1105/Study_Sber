c0d01557f7f0cf0ba0d0a044ec7074d61dc8b4e0
Pivovarova_Anastasia <serdyuchenko.n@mail.ru>
2021-03-12 10:29:40 +0300

    Delete CollectionSimpleExample.java

--- src/study/lesson3/generics/CollectionSimpleExample.java
+++
@@ -1,9 +0,0 @@
-package study.lesson3.generics;
-
-import java.util.List;
-
-public class CollectionSimpleExample {
-    public void process(List<Integer> list) {
-        Integer o = list.get(0);
-    }
-}
